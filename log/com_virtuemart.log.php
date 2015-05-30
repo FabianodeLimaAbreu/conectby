f800fec45b77b6849c28eda29ea3bc80250434db
FabianodeLimaAbreu <fabianodelimaabreu@gmail.com>
2015-05-30 00:38:49 -0300

    Commit Inicial V

---
+++ log/com_virtuemart.log.php
@@ -0,0 +1,7 @@
+#
+#<?php die("Forbidden."); ?>
+
+2014-11-21 17:28:08 ERROR vmError: The %1$s is not writeable. Please ensure that you have the correct file permissions and directory settings
+2014-11-21 17:28:25 ERROR vmError: The %1$s is not writeable. Please ensure that you have the correct file permissions and directory settings
+2014-11-21 17:28:26 ERROR vmError: The %1$s is not writeable. Please ensure that you have the correct file permissions and directory settings
+2015-05-30 02:12:56 ERROR vmError: Warning, the Safe Path is <b>not accessible</b> (does not exist or no permission), for safety reasons it is very important to create a folder in a path not accessible by an URL, create also a folder invoices in it to store your sensitive data secure. Our suggested path is for your system C:\xampp\htdocs\vmfiles, use this link to the <a href='http://localhost:3000/conectby/administrator/index.php?option=com_virtuemart&view=config' >config</a>
---
+++ log/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-11-24 09:27:05 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-11-24	09:27:05	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2014-11-24	09:28:10	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2014-11-24	09:28:41	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
