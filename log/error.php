4312f65aceae3153e7836ece3e95d0b787480ad0
adrielli-fernandes <adrielli-fernandes@live.com>
2015-05-31 13:27:37 -0300

    novo bd

--- log/error.php
+++ log/error.php
@@ -7,3 +7,5 @@
 2014-11-24	09:27:05	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2014-11-24	09:28:10	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2014-11-24	09:28:41	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-05-31	15:57:41	INFO	127.0.0.1	Joomla CANCELED: 	
+2015-05-31	15:58:06	INFO	127.0.0.1	Joomla CANCELED: 	
