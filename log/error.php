6235afa3174676b802ff54c335fd9aa68bd76a85
FabianodeLimaAbreu <fabianodelimaabreu@gmail.com>
2015-06-22 00:20:49 -0300

    new

--- log/error.php
+++ log/error.php
@@ -9,3 +9,13 @@
 2014-11-24	09:28:41	INFO	89.70.187.168	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2015-05-31	15:57:41	INFO	127.0.0.1	Joomla CANCELED: 	
 2015-05-31	15:58:06	INFO	127.0.0.1	Joomla CANCELED: 	
+2015-06-22	03:12:27	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:12:32	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:12:36	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:12:46	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:12:50	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:13:22	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:13:48	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:13:51	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:14:01	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-06-22	03:14:11	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
