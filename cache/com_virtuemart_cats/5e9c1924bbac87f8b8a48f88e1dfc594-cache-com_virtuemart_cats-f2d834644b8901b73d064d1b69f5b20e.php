9e036a9610bae417cecd8c71ca91528811e13c9b
Fabiano de Lima Abreu <fabiano.de.lima@bol.com.br>
2015-06-23 18:55:46 -0300

    Daily

---
+++ cache/com_virtuemart_cats/5e9c1924bbac87f8b8a48f88e1dfc594-cache-com_virtuemart_cats-f2d834644b8901b73d064d1b69f5b20e.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";a:1:{i:0;O:8:"stdClass":8:{s:22:"virtuemart_category_id";s:2:"11";s:13:"category_name";s:9:"Camisetas";s:20:"category_description";s:27:"<p>Camisetas Masculinas</p>";s:8:"metadesc";s:0:"";s:7:"metakey";s:0:"";s:11:"customtitle";s:0:"";s:4:"slug";s:9:"camisetas";s:19:"virtuemart_media_id";b:0;}}}
